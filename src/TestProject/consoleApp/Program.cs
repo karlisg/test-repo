cf2343eaa97ae94dabf775dc8a4466502d02f08c
Karla Saavedra <karlita.saavedra@gmail.com>
2022-12-02 13:41:00 -0400

    add class

--- src/TestProject/consoleApp/Program.cs
+++ src/TestProject/consoleApp/Program.cs
@@ -1,6 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using consoleApp;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, rpt1");
 Console.WriteLine("Hello, rpt2!");
+Console.WriteLine("Hello, rpt3");
+
+Class1 c = new Class1();
+c.sayBye();
